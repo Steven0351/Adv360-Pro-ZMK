fd3dc9c2c4b400c049fcc826f5eca8c4b9c0c575
Steven Sherry <steven.r.sherry@gmail.com>
2025-01-22 11:14:09 -0600

    radical changes. trying to mimic my imprint on the advantage 360 before it arrives

---
+++ config/keydef.h
@@ -0,0 +1,91 @@
+#define LU0 0
+#define LU1 1
+#define LU2 2
+#define LU3 3
+#define LU4 4
+#define LU5 5
+#define LU6 6
+
+#define RU0 7
+#define RU1 8
+#define RU2 9
+#define RU3 10
+#define RU4 11
+#define RU5 12
+#define RU6 13
+
+#define LT0 14
+#define LT1 15
+#define LT2 16
+#define LT3 17
+#define LT4 18
+#define LT5 19
+#define LT6 20
+
+#define RT0 21
+#define RT1 22
+#define RT2 23
+#define RT3 24
+#define RT4 25
+#define RT5 26
+#define RT6 27
+
+#define LM0 28
+#define LM1 29
+#define LM2 30
+#define LM3 31
+#define LM4 32
+#define LM5 33
+#define LM6 34
+
+#define RM0 39
+#define RM1 40
+#define RM2 41
+#define RM3 42
+#define RM4 43
+#define RM5 44
+#define RM6 45
+
+#define LB0 46
+#define LB1 47
+#define LB2 48
+#define LB3 49
+#define LB4 50
+#define LB5 51
+
+#define RB0 54
+#define RB1 55
+#define RB2 56
+#define RB3 57
+#define RB4 58
+#define RB5 59
+
+#define LD0 60
+#define LD1 61
+#define LD2 62
+#define LD3 63
+#define LD4 64
+
+#define RD0 71
+#define RD1 72
+#define RD2 73
+#define RD3 74
+#define RD4 75
+
+#define LH0 35
+#define LH1 36
+#define LH2 52
+#define LH3 65
+#define LH4 66
+#define LH5 67
+
+#define RH0 37
+#define RH1 38
+#define RH2 53
+#define RH3 68
+#define RH4 69
+#define RH5 70
+
+#define LKEYS LU0 LU1 LU2 LU3 LU4 LU5 LU6 LT0 LT1 LT2 LT3 LT4 LT5 LT6 LM0 LM1 LM2 LM3 LM4 LM5 LM6 LB0 LB1 LB2 LB3 LB4 LB5 LD0 LD1 LD2 LD3 LD4
+#define RKEYS RU0 RU1 RU2 RU3 RU4 RU5 RU6 RT0 RT1 RT2 RT3 RT4 RT5 RT6 RM0 RM1 RM2 RM3 RM4 RM5 RM6 RB0 RB1 RB2 RB3 RB4 RB5 RD0 RD1 RD2 RD3 RD4
+#define THUMBS LH0 LH1 LH2 LH3 LH4 LH5 RH0 RH1 RH2 RH3 RH4 RH5
